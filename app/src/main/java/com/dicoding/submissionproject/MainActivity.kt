27fca57d9e0d701cec11e0d45bf15b8a840a3396
Yazid Bintang <yazidbintang2711@gmail.com>
2023-12-08 15:45:04 +0700

    Add menu logic

--- app/src/main/java/com/dicoding/submissionproject/MainActivity.kt
+++ app/src/main/java/com/dicoding/submissionproject/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.dicoding.submissionproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val intent = Intent(this, AboutActivity::class.java)
+        startActivity(intent)
+        return super.onOptionsItemSelected(item)
     }
 }
